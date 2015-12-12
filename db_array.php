992bcc5754eef3e5c6ddd350371db6f9ee344d0f
SunnyAnn <anya96154@gmail.com>
2015-12-12 16:31:59 +0200

    Create db_array.php

---
+++ db_array.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$data = array(
+	'contact' => array(
+		array('post')
+		array('telephone number')
+	),
+	'cost of courses' => array(
+        array('level'),
+        array('duration'),
+        array('price'),
+    ),
+    'data' => 'information about a teacher',
+    'lessons' => 'course program',
+    'title' => 'Курсы Php/Mysql',
+    'transition' => 'Главная страница',
+);
